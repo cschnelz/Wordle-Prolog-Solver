469296d0ea1b963bc743eb07c773b8f8a612703e
Carlos Garcia <91993499+garciale24@users.noreply.github.com>
2022-02-16 13:45:30 -0800

    WordScript

---
+++ pythonWordScript.py
@@ -0,0 +1,35 @@
+from typing import List
+import os
+
+def main():
+    input: str = ""
+    words: List[str] = []
+
+    f = open("words.pl", "w")
+    f.write(":-dynamic word/1.\n")
+    
+    # opening the text file
+    with open('words.txt','r') as file:
+    
+        # reading each line    
+        for line in file:
+    
+            # reading each word        
+            for word in line.split():          
+                input = "word(["
+                for letterIdx in range(len(word)):
+                    input += word[letterIdx]
+                    if letterIdx != len(word)-1:
+                        input += ","
+                input += "]).\n"
+                words.append(input)
+
+    # get rid of last newline in final file
+    words[-1] = words[-1][0:-1]
+    for word in words:
+        f.write(word)
+    f.close()
+
+if __name__ == "__main__":
+    main()
+
